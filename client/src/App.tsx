f37ee0bd791411b34cc094d316672e3ac853cf68
eucliide <eucyusagashugi@gmail.com>
2026-07-29 09:18:41 +0200

    feat(app): start transition after meeting

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,12 @@
 import { MarketingPage } from "@/features/marketing/pages/MarketingPage";
+import { useTransitionCoordinator } from "@/features/transition/hooks/useTransitionCoordinator";
 
 function App() {
+  const {
+    transition,
+    startTransition,
+    finishTransition,
+  } = useTransitionCoordinator();
   return <MarketingPage />;
 }
 
