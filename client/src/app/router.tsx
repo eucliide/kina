e6e8fcc919189f4e88807daf29f179a6b4237e21
eucliide <eucyusagashugi@gmail.com>
2026-08-22 23:06:38 +0200

    fix(router): align application route flow

--- client/src/app/router.tsx
+++ client/src/app/router.tsx
@@ -1,4 +1,9 @@
-import { createBrowserRouter } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Navigate,
+} from "react-router-dom";
+
+import { AuthLayout } from "@/app/AuthLayout";
 
 import App from "@/App";
 
@@ -6,69 +11,70 @@ import { JoinPage } from "@/features/join/pages/JoinPage";
 import { NamePage } from "@/features/join/pages/NamePage";
 import { LobbyPage } from "@/features/lobby/pages/LobbyPage";
 import { MeetingPage } from "@/features/meeting/pages/MeetingPage";
-import { ReflectionPage } from "@/features/reflection/pages/ReflectionPage";
 import { CreatePage } from "@/features/create/pages/CreatePage";
 import { WnrsPage } from "@/features/wnrs/pages/WnrsPage";
 import { GatherAroundPage } from "@/features/gather-around/pages/GatherAroundPage";
 import { TableTopicsPage } from "@/features/tabletopics/pages/TableTopicsPage";
 import { SecretMissionPage } from "@/features/mission/pages/SecretMissionPage";
 
-export const router =
-  createBrowserRouter([
-    {
-      path: "/",
-      element: <App />,
-    },
+export const router = createBrowserRouter([
+  {
+    element: <AuthLayout />,
+    children: [
+      {
+        path: "/",
+        element: <App />,
+      },
 
-    {
-      path: "/create",
-      element: <CreatePage />,
-    },
+      {
+        path: "/create",
+        element: <CreatePage />,
+      },
 
-    {
-      path: "/join",
-      element: <JoinPage />,
-    },
+      {
+        path: "/join",
+        element: <JoinPage />,
+      },
 
-    {
-      path: "/join/name",
-      element: <NamePage />,
-    },
+      {
+        path: "/join/name",
+        element: <NamePage />,
+      },
 
-    {
-      path: "/secret-mission",
-      element: (
-        <SecretMissionPage
-          onContinue={() => {
-            window.location.href = "/lobby";
-          }}
-        />
-      ),
-    },
+      {
+        path: "/secret-mission",
+        element: <SecretMissionPage />,
+      },
 
-    {
-      path: "/lobby",
-      element: <LobbyPage />,
-    },
+      {
+        path: "/lobby",
+        element: <LobbyPage />,
+      },
 
-    {
-      path: "/meeting",
-      element: <MeetingPage />,
-    },
-    {
-      path: "/wnrs",
-      element: <WnrsPage />,
-    },
-    {
-      path: "/gather-around",
-      element: <GatherAroundPage />,
-    },
-    {
-          path: "/tabletopics",
-          element: <TableTopicsPage />,
-        },
-    {
-      path: "/reflection",
-      element: <ReflectionPage />,
-    },
-  ]);
+      {
+        path: "/meeting",
+        element: <MeetingPage />,
+      },
+
+      {
+        path: "/wnrs",
+        element: <WnrsPage />,
+      },
+
+      {
+        path: "/gather-around",
+        element: <GatherAroundPage />,
+      },
+
+      {
+        path: "/tabletopics",
+        element: <TableTopicsPage />,
+      },
+
+      {
+        path: "/reflection",
+        element: <Navigate to="/lobby" replace />,
+      },
+    ],
+  },
+]);
